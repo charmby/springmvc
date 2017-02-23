3e91e9b02ecf52f110be7f1d6a18550cba7afb19
xiaoyiyibaibai <2367808698@qq.com>
2017-02-23 17:05:24 +0800

    添加删除的内容和修改的呢绒

---
+++ springmvc/springmvccontroller/src/main/java/com/gsafety/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.gsafety.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ * 开发处理器/页面控制器
+ * @author Thinkpad
+ *
+ */
+public class HelloWorldController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
