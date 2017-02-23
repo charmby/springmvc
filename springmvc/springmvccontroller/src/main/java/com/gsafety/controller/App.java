476a7bff39853685f5c0cb89b6ff78f92160bcdb
xiaoyiyibaibai <2367808698@qq.com>
2017-02-23 16:02:51 +0800

    提交Controller的项目

---
+++ springmvc/springmvccontroller/src/main/java/com/gsafety/controller/App.java
@@ -0,0 +1,13 @@
+package com.gsafety.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
