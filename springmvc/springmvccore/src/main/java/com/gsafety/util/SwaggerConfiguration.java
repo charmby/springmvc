d4f02eb9d540aa70dfd72323f235cf945921b67f
xiaoyiyibaibai <2367808698@qq.com>
2017-03-23 09:41:47 +0800

    删除无用的注释信息。

--- springmvc/springmvccore/src/main/java/com/gsafety/util/SwaggerConfiguration.java
+++ springmvc/springmvccore/src/main/java/com/gsafety/util/SwaggerConfiguration.java
@@ -16,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc      // 启用Mvc //非springboot框架需要引入注解@EnableWebMvc。不配置这个不能扫描，Controller中的request的url请求
 @EnableSwagger2  //@Configuration和@EnableSwagger2是最少的两个注解。并且配置后，还需要在springmvc的xml文件中配置。该类的bean
 public class SwaggerConfiguration {
-
 	@Bean
 	public Docket getApiInfo() {
 		return new Docket(DocumentationType.SWAGGER_2)
@@ -26,11 +25,8 @@ public class SwaggerConfiguration {
 				.paths(PathSelectors.any())  // 对所有路径进行监控
 				.build()
 				.apiInfo(outApiInfo());
-
 	}
-
 	
-
 	private ApiInfo outApiInfo() {
 		return new ApiInfo(
 				"springmvc的各项验证信息", // title 标题
@@ -38,21 +34,8 @@ public class SwaggerConfiguration {
 				"1.0.0", // version
 				"http://mylearn/*", // termsOfService
 				new Contact("肖东红","","xiaodonghong@gsafety.com"), // contact
-				"Apache 2.0", // licence
+				"北京辰安科技股份有限公司", // licence
 				"http://www.apache.org/licenses/LICENSE-2.0.html" // licence url
 				);
-
 	}
-
-/*	@Bean
-	public UiConfiguration getUiConfig() {
-		return new UiConfiguration(
-				null,// url,暂不用
-				"none",       // docExpansion          => none | list
-				"alpha",      // apiSorter             => alpha
-				"schema",     // defaultModelRendering => schema
-				UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS,
-				true,        // enableJsonEditor      => true | false
-				false);        // showRequestHeaders    => true | false
-	}*/
 }
