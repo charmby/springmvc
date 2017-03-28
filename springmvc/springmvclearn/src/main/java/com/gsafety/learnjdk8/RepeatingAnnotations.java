e1ef6463b165bbc059b4b236c6fe0c8e8631cc31
xiaoyiyibaibai <2367808698@qq.com>
2017-03-28 20:27:22 +0800

    测试内容信息。

--- springmvc/springmvclearn/src/main/java/com/gsafety/learnjdk8/RepeatingAnnotations.java
+++ springmvc/springmvclearn/src/main/java/com/gsafety/learnjdk8/RepeatingAnnotations.java
@@ -6,7 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.slf4j.Logger;
+
+
+
 public class RepeatingAnnotations {
+private static Logger loger= org.slf4j.LoggerFactory.getLogger(RepeatingAnnotations.class);
 	@Target( ElementType.TYPE )
 	@Retention( RetentionPolicy.RUNTIME )
 	public @interface Filters {
@@ -18,11 +23,12 @@ public class RepeatingAnnotations {
 	public @interface Filter {
 		String value();
 	};
+	
 	@Filter( "filter1" )
 	@Filter( "filter2" )
 	public interface Filterable {
 	}
 	public static void main(String[] args) {
 		for( Filter filter: Filterable.class.getAnnotationsByType( Filter.class ) ) {
-			System.out.println( filter.value());
+			loger.debug(filter.value());
 		}}}
