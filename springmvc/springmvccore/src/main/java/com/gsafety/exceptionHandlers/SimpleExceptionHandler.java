acaeefc8b00b968b804b9c40b8e0d68d71c238ed
xiaoyiyibaibai <2367808698@qq.com>
2017-03-31 15:32:43 +0800

    返回异常信息给页面中。

--- springmvc/springmvccore/src/main/java/com/gsafety/exceptionHandlers/SimpleExceptionHandler.java
+++ springmvc/springmvccore/src/main/java/com/gsafety/exceptionHandlers/SimpleExceptionHandler.java
@@ -1,15 +1,11 @@
 package com.gsafety.exceptionHandlers;
 
-import java.util.Collection;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import com.gsafety.UnknownResourceException;
 
@@ -22,10 +18,11 @@ public class SimpleExceptionHandler implements HandlerExceptionResolver {
     	ModelAndView module = 	new ModelAndView();
     	if(ex instanceof UnknownResourceException) {
     		module.addObject("status", "444");
-    		module.addObject("message", ex.getMessage());
+    		module.addObject("exceptionMessage", ex.getMessage());
     		
     		}
     	response.setContentType("application/json;charset=utf-8;");
+    	module.setViewName("exception");
         return module;   
     }   
   
