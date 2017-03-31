381a643a3f5a47e876f0fd13b27ad24642ba53d6
xiaoyiyibaibai <2367808698@qq.com>
2017-03-31 14:54:05 +0800

    cookie的国际化。所谓国际化，就是通过后台获得国际化后的文件，然后传入前台。优先级比浏览器国际化高。

--- springmvc/springmvccontroller/src/main/java/com/gsafety/controller/GlobalController.java
+++ springmvc/springmvccontroller/src/main/java/com/gsafety/controller/GlobalController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.support.RequestContext;
 
@@ -104,5 +106,50 @@ public class GlobalController {
 		}
 		return "globaltest2";
 	}
+	
+	/**
+	 * <bean id="localeResolver" class="org.springframework.web.servlet.i18n.SessionLocaleResolver" />
+注释掉，并添加以下内容：
+
+<bean id="localeResolver" class="org.springframework.web.servlet.i18n.CookieLocaleResolver" />
+	 * @param request
+	 * @param model
+	 * @param langType
+	 * @return
+	 */
+	 @RequestMapping(value="/test3", method = {RequestMethod.GET})
+	    public String test3(HttpServletRequest request, HttpServletResponse response, Model model, @RequestParam(value="langType", defaultValue="zh") String langType){
+	        if(!model.containsAttribute("contentModel")){
+	            
+	            if(langType.equals("zh")){
+	                Locale locale = new Locale("zh", "CN"); 
+	                //request.getSession().setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,locale);
+	                (new CookieLocaleResolver()).setLocale (request, response, locale);
+	            }
+	            else if(langType.equals("en")){
+	                Locale locale = new Locale("en", "US"); 
+	                //request.getSession().setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,locale);
+	                (new CookieLocaleResolver()).setLocale (request, response, locale);
+	            }
+	            else 
+	                //request.getSession().setAttribute(SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME,LocaleContextHolder.getLocale());
+	                (new CookieLocaleResolver()).setLocale (request, response, LocaleContextHolder.getLocale());
+	            
+	            //从后台代码获取国际化信息
+	            RequestContext requestContext = new RequestContext(request);
+	            model.addAttribute("money", requestContext.getMessage("money"));
+	            model.addAttribute("date", requestContext.getMessage("date"));
+
+	            
+	            FormatModel formatModel=new FormatModel();
+
+	            formatModel.setMoney(12345.678);
+	            formatModel.setDate(new Date());
+	            
+	            model.addAttribute("contentModel", formatModel);
+	        }
+	        return "globaltest3";
+	    }
+
 
 }
