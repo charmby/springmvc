014ca38d684564fc93d75bc5eff642ff68c2ecec
xiaoyiyibaibai <2367808698@qq.com>
2017-04-09 16:38:49 +0800

    添加log4j的配置信息。

--- springmvc/springmvccore/src/main/java/com/gsafety/util/swaggerconfig/SwaggerConfiguration.java
+++ springmvc/springmvccore/src/main/java/com/gsafety/util/swaggerconfig/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
-package com.gsafety.util;
+package com.gsafety.util.swaggerconfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,8 +18,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc      // 启用Mvc //非springboot框架需要引入注解@EnableWebMvc。不配置这个不能扫描，Controller中的request的url请求
 @EnableSwagger2  //@Configuration和@EnableSwagger2是最少的两个注解。并且配置后，还需要在springmvc的xml文件中配置。该类的bean
 public class SwaggerConfiguration {
+	private static Logger logger  = LoggerFactory.getLogger(SwaggerConfiguration.class);
 	@Bean
 	public Docket getApiInfo() {
+		logger.warn("配置了名称为：项目所有Restfule接口 的分组。 过滤了所有的rest请求。");
 		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("项目所有Restfule接口")
 				.select()   //选择哪些路径和API会生成document
@@ -28,6 +32,7 @@ public class SwaggerConfiguration {
 	}
 	
 	private ApiInfo outApiInfo() {
+		logger.warn("swagger的各项信息。");
 		return new ApiInfo(
 				"springmvc的各项验证信息", // title 标题
 				"springmvc的各项外部接口信息", // description 描述 标题下
@@ -40,6 +45,7 @@ public class SwaggerConfiguration {
 	}
 	@Bean  
     public Docket createRestApi() {  
+		logger.warn("配置了名称为：测试类Restful接口  的分组。 过滤了所有com.gsafety.bak.controller中的的rest请求！。");
         return new Docket(DocumentationType.SWAGGER_12)  
         		 .groupName("测试类Restful接口")
                 .apiInfo(outApiInfo())  
@@ -51,6 +57,7 @@ public class SwaggerConfiguration {
 	
 	@Bean
 	public Docket getHelloWorldApiInfo() {
+		logger.warn("配置了名称为：仅展示示例欢迎类的Restfule接口 的分组。 过滤了所有com.gsafety.controller中的的rest请求！。");
 		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("仅展示示例欢迎类的Restfule接口")
 				.select()   //选择哪些路径和API会生成document
