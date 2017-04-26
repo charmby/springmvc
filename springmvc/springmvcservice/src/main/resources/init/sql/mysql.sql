197a0a929f21dba8f0b43a20a5f7a4f234acd8d6
xiaoyiyibaibai <2367808698@qq.com>
2017-04-26 22:02:20 +0800

    数据库脚本信息

--- springmvc/springmvcservice/src/main/resources/init/sql/mysql.sql
+++ springmvc/springmvcservice/src/main/resources/init/sql/mysql.sql
@@ -1,5 +1,8 @@
+create database demo;
+use demo;
+
 CREATE TABLE `user_t` ( 
-`id`int(11) primary key ,  
+`id`int(11) primary key  AUTO_INCREMENT,  
 `user_name` varchar(255) DEFAULT NULL,  
 `password` varchar(255) ,
 `age` int(11)
@@ -8,9 +11,14 @@ CREATE TABLE `user_t` (
 insert into user_t values(1,'xiaodonghong','xiaodonghong',20);
 
 
+CREATE TABLE person(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT,address varchar(200),telphone varchar(20),sex int,note varchar(255))CHARSET=utf8;
+INSERT INTO person(NAME, age,address,telphone,sex) VALUES('孤傲苍狼', 27,"北京市海淀区洪城路223号","18765906543",1);
+INSERT INTO person(NAME, age,address,telphone,sex) VALUES('白虎神皇', 27,"上海市滨海新区34路","18609876554",0);
 
-create database mybatis;
-use mybatis;
-CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20), age INT);
-INSERT INTO users(NAME, age) VALUES('孤傲苍狼', 27);
-INSERT INTO users(NAME, age) VALUES('白虎神皇', 27);
+
+
+CREATE TABLE sextype(id INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(20) )CHARSET=utf8;
+INSERT INTO sextype(NAME) VALUES('男');
+INSERT INTO sextype(NAME) VALUES('女');
+INSERT INTO sextype(NAME) VALUES('未知');
+
