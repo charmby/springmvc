e01b555331a190302ee74309eeabed75a6eb718c
xiaoyiyibaibai <2367808698@qq.com>
2017-05-25 14:05:21 +0800

    如果定义了过滤规则。的filters。则用到的都要添加一下。

---
+++ springmvc/springmvcshiro/src/main/java/com/gsafety/shiro/filter/MyFormAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.gsafety.shiro.filter;
+
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+
+public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
+
+}
