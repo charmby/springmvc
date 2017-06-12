6d29fc7c6be4c0b6877f1bd0838c02f6e1337f4e
xiaoyiyibaibai <2367808698@qq.com>
2017-06-12 15:13:51 +0800

    增加处理信息。。

---
+++ springmvc/springmvcresthttpclient/src/main/java/com/gsafety/httpclient/controller/MsgRestControllerTest.java
@@ -0,0 +1,69 @@
+package com.gsafety.httpclient.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import org.junit.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+
+import com.gsafety.httpclient.utils.HttpRestUtils;
+@RestController
+@RequestMapping("/httpclient")
+@Api(value = "使用httpclient进行调用", description = "有关于httpclient管理的操作", position = 1)  
+public class MsgRestControllerTest {
+
+	@ApiOperation(value = "获得所有用户信息", httpMethod = "post", produces = "application/json")
+	@ApiResponse(code = 200, message = "success")
+	@ResponseBody
+	@RequestMapping(value = "/testPostMethod", method = RequestMethod.POST, produces = "application/json")
+	public String testPostMethod(){
+		/*        RealTimeMsg msg = new RealTimeMsg();
+        msg.setFrom("001");*/
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("client_id", "mobile");
+		map.put("client_secret", "mobile");
+		map.put("grant_type", "password");
+		map.put("scope", "read");
+		map.put("username", "test");
+		map.put("password", "test");
+
+		/*   ?client_id=test&client_secret=test&grant_type=password&scope=read write&username=test&password=test*/
+		String resp = HttpRestUtils.post("http://localhost:8080/authz/oauth/token", JSON.toJSONString(map));
+		return resp;
+	}
+
+	@ApiOperation(value = "使用其他方式连接zzl", httpMethod = "post", produces = "application/json")
+	@ApiResponse(code = 200, message = "success")
+	@ResponseBody
+	@RequestMapping(value = "/testzzlPost", method = RequestMethod.POST, produces = "application/json")
+	public void testzzlPost(){
+		HttpRestUtils.zzlPost();
+		/*       String resp = HttpRestUtils.post("http://localhost:6080/cloud-push-server/realTimeMessage", JSONUtil.toJSONString(msg));
+        System.out.println(resp);*/
+	}
+
+	@Test
+	public void testCancelMsg(){
+		String resp = HttpRestUtils.get("http://localhost:6080/cloud-push-server/cancelMsg/09876000");
+		System.out.println(resp);
+	}
+
+
+	@Test
+	public void test() throws ParseException{
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println(sdf.parse("2099-12-31").getTime());
+	}
+}
--- springmvc/springmvcresthttpclient/src/main/java/com/gsafety/httpclient/utils/HttpRestUtils.java
+++ springmvc/springmvcresthttpclient/src/main/java/com/gsafety/httpclient/utils/HttpRestUtils.java
@@ -1,9 +1,38 @@
 package com.gsafety.httpclient.utils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+
+
+
+
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 /**
  * @author huangll
  *
@@ -32,4 +61,79 @@ public class HttpRestUtils {
 			throw new RuntimeException(e);
 		}
 	}
-}
+
+
+	public static   void zzlPost(){
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpPost httpPost = new HttpPost("http://localhost:8080/authz/oauth/token");
+			List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+			nvps.add(new BasicNameValuePair("client_id", "mobile"));
+			nvps.add(new BasicNameValuePair("client_secret", "mobile"));
+
+			nvps.add(new BasicNameValuePair("grant_type", "password"));
+
+			nvps.add(new BasicNameValuePair("scope", "read"));
+			nvps.add(new BasicNameValuePair("username", "test"));
+			nvps.add(new BasicNameValuePair("password", "test"));
+
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+			CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+
+			try {
+				System.out.println(response2.getStatusLine());
+				HttpEntity entity2 = response2.getEntity();
+				// do something useful with the response body
+				// and ensure it is fully consumed
+				//EntityUtils.consume(entity2);
+
+				if (entity2 != null) {  
+					// 打印响应内容长度    
+					System.out.println("Response content length: " + entity2.getContentLength());  
+					// 打印响应内容    
+					//System.out.println("Response content: " + EntityUtils.toString(entity2));  
+
+
+					ContentType contentType = ContentType.get(entity2);
+					System.out.println("Mime Type: " + contentType.getMimeType());
+
+					if(contentType.toString().equalsIgnoreCase(ContentType.APPLICATION_JSON.toString())){
+						//HttpClientTest test = new HttpClientTest();
+						String content = EntityUtils.toString(entity2);
+						//test.toString(entity2,contentType);
+						System.out.println("Response content: " + content);
+
+						Object obp =   JSON.parse(content);
+						Object object2 =   JSON.toJSON(content);
+						/*JsonParser*/
+						JsonParser parser=new JsonParser();  //创建JSON解析器
+						JsonObject object=(JsonObject) parser.parse(content);  //创建JsonObject对象
+
+						System.out.println("-------------------------------------------------------"); //将json数据转为为String型的数据
+
+						System.out.println("access_token="+object.get("access_token").getAsString()); //将json数据转为为String型的数据
+						System.out.println("refresh_token="+object.get("refresh_token").getAsString()); //将json数据转为为String型的数据
+						System.out.println("token_type="+object.get("token_type").getAsString()); //将json数据转为为String型的数据
+						System.out.println("expires_in="+object.get("expires_in").getAsLong()); //将json数据转为为String型的数据                      
+					}
+
+				}  
+
+			} finally {
+				response2.close();
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				httpclient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}}
