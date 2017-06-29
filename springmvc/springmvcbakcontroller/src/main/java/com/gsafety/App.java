79e84066c59e21f205178beb41422ebaa7fc7d94
xiaoyiyibaibai <2367808698@qq.com>
2017-06-29 18:13:18 +0800

    发送信息。。。

--- springmvc/springmvcbakcontroller/src/main/java/com/gsafety/App.java
+++ springmvc/springmvcbakcontroller/src/main/java/com/gsafety/App.java
@@ -1,13 +1,21 @@
 package com.gsafety;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws MalformedURLException
     {
+    	
+    	
+    	URL url = new URL("http://www.sojson.com/yasuoyihang.html");
+    	String domain = url.getHost();
+    	System.out.println("域名：" + domain);
         System.out.println( "Hello World!" );
     }
 }
